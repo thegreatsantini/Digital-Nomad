b72b1b1404398fa4b52c4761f0859c49af7b1dca
thegreatsantini <lss13@my.fsu.edu>
2018-06-26 11:35:37 -0700

    users can add contacts

--- routes/profile.js
+++ routes/profile.js
@@ -6,32 +6,38 @@ const bodyParser = require('body-parser')
 
 
 const db = require('../models/user');
-const contacts = require('../models/user')
+const contacts = require('../models/contacts')
 // const SavedRecipe = require('../models/savedRecipes')
 
 profileRoute.get('/', isLoggedIn, function (req, res) {
-    // console.log(res.locals.currentUser)
     res.render('profile');
 });
 
-profileRoute.get('/addressbook', function (req, res) {
+profileRoute.get('/addressbook', isLoggedIn, function (req, res) {
     res.render('addressBook')
 });
 
 profileRoute.post('/addressbook', function(req, res, next){
-    
-
     db.findById(res.locals.currentUser.id, (err, success) => {
-        SavedRecipe.create(req.body, (error, recipe) => {
-            if (error) {
-                res.status(500).send()
+        // Check for repeats
+        contacts.find({ title: req.body.title }, (fail, item) => {
+            // if the item already exits
+            if (item.length > 0 ) {
+                return res.send('recipes already exits')
             }
-            success.saved.push(recipe);
-            success.save();
+
+            contacts.create(req.body, (error, contact) => {
+                if (error) {
+                    return res.status(500).send()
+                }
+                success.savedContacts.push(contact);
+                console.log(success)
+                success.save().then(() => {
+                    return res.send('success')
+                });
+            })
         })
     });
-    res.send('success')
-
 })
 
 
@@ -39,51 +45,8 @@ profileRoute.get('/createcard', isLoggedIn, function(req, res){
     res.render('createcard')
 })
 
-profileRoute.post('/', function (req, res) {
-
-    let newBookmark = {
-        title: req.body.title,
-        publisher: req.body.publisher,
-        image: req.body.image,
-        source: req.body.source
-    };
-
-    db.findById(res.locals.currentUser.id, (err, success) => {
-        SavedRecipe.create(req.body, (error, recipe) => {
-            if (error) {
-                res.status(500).send()
-            }
-            success.saved.push(recipe);
-            success.save();
-        })
-    });
-    res.send('success')
-})
-
-profileRoute.delete('/', function (req, res) {
-    db.findById(res.locals.currentUser.id, (err, user) => {
-
-        let saved_id;
-
-        const removeMe = user.saved.filter(food => {
-            return food.title === bookMarkToRemove.title
-        })
-
-        saved_id = removeMe[0]._id;
 
 
-        SavedRecipe.findByIdAndRemove(saved_id, (error, success) => {
-            if (error) {
-                res.status(500).send()
-            }
-            res.status(200).send()
-        })
-        db.findOne({ name: res.locals.currentUser.name }, (fail, loggedInUser) => {
-            loggedInUser.saved.id(saved_id).remove()
-            loggedInUser.save()
-            res.status(200).send()
-        })
-    })
-})
+
 
 module.exports = profileRoute;
